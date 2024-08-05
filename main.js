f07be1e732303b54ae33c02d45ebe0474e09b1d1
Karinagonzalez19 <karina191292@hotmail.com>
2024-08-05 08:55:35 -0500

    agregue contenido tres

--- main.js
+++ main.js
@@ -21,6 +21,37 @@ const conversor = ()=>{
 }
 dolar.addEventListener("input", conversor)
 conversorPeso.addEventListener("input", conversorPeso)
-
-
-
+//3
+const tareas = [
+    {
+      id: 1,
+      titulo: 'Comprar groceries',
+      texto: 'Ir al supermercado y comprar frutas, verduras y pan.',
+      realizada: false
+    },
+    {
+      id: 2,
+      titulo: 'Hacer ejercicio',
+      texto: 'Hacer una sesión de yoga de 30 minutos en casa.',
+      realizada: true
+    },
+    {
+      id: 3,
+      titulo: 'Llamar al médico',
+      texto: 'Programar una cita con el médico para un chequeo anual.',
+      realizada: false
+    },
+    {
+      id: 4,
+      titulo: 'Leer un libro',
+      texto: 'Leer al menos 50 páginas del libro que estoy empezando.',
+      realizada: true
+    },
+    {
+      id: 5,
+      titulo: 'Preparar presentación',
+      texto: 'Crear las diapositivas y practicar la presentación para la reunión de mañana.',
+      realizada: false
+    },
+  ];
+let idGlobal =5
